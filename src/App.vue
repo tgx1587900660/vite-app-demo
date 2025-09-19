30a6dc181f24b85e0350112cee807b357b7dd7bd
tangganxiao <tangganxiao@makeblock.com>
2025-09-19 16:10:34 +0800

    feat: 弹窗组件

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <template>
   <h1 class="text-center text-2xl">App 根组件</h1>
-  <router-view></router-view>
+  <n-modal-provider>
+    <router-view></router-view>
+  </n-modal-provider>
 </template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,51 @@
 <template>
-  <n-space>
-    <n-button>Default</n-button>
-    <n-button type="tertiary"> Tertiary </n-button>
-    <n-button type="primary"> Primary </n-button>
-    <n-button type="info"> Info </n-button>
-    <n-button type="success"> Success </n-button>
-    <n-button type="warning"> Warning </n-button>
-    <n-button type="error"> Error </n-button>
-  </n-space>
+  <div class="flex justify-center mt-6">
+    <n-button @click="showModal4 = !showModal4"> 弹窗嵌套 </n-button>
+  </div>
+
+  <n-modal class="xxxxxxxxxxx" :on-after-leave="onAfterLeave" ref="modalRef" v-model:show="showModal4" arrow-wrapper-class title="嵌套弹窗拖拽" :show-mask="false" preset="card" draggable :style="{ width: '400px' }" @close="onClose">
+    <div class="flex justify-end">
+      <div class="w-6 h-6 bg-[deepskyblue]" @click="onShowPopover"></div>
+      <n-popover class="my-self-selector" :show="showPopover" :show-arrow="false" placement="right" :x="x" :y="y" trigger="manual">
+        <span>showPopover: {{ showPopover }}</span>
+        <div class="large-text">
+          <img class="w-[300px] object-cover" src="https://picsum.photos/id/237/200" alt="图片" />
+        </div>
+      </n-popover>
+    </div>
+  </n-modal>
 </template>
 
-<script>
-export default {};
-</script>
+<script setup lang="ts">
+import { ref, useTemplateRef } from 'vue';
 
-<style></style>
+const showPopover = ref(false);
+const showModal4 = ref(false);
+const modalRef = useTemplateRef('modalRef');
+const x = ref(0);
+const y = ref(0);
+
+function onClose() {
+  showPopover.value = false;
+  showModal4.value = false;
+}
+
+function onAfterLeave() {
+  showPopover.value = false;
+}
+
+function onShowPopover() {
+  getModalPosition();
+  showPopover.value = !showPopover.value;
+}
+
+function getModalPosition() {
+  console.log('modalRef.value :>> ', modalRef.value);
+  const modalEl = document.querySelector('.xxxxxxxxxxx') as HTMLElement;
+  const rect = modalEl?.getBoundingClientRect();
+  x.value = rect?.x + rect?.width;
+  y.value = rect?.y + 170;
+  console.log('x.value :>> ', x.value);
+  console.log('y.value :>> ', y.value);
+}
+</script>
